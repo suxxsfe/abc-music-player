396a08392b6a0c403f1692d88b70eca2c1037ab6
suxxsfe <suxxsfe@gmail.com>
2023-12-25 17:47:46 +0800

    abc sample to parse

--- src/abc/Main.java
+++ src/abc/Main.java
@@ -3,7 +3,7 @@ package abc;
 public class Main{
 	public static void main(String argv[]){
         String abc = "X:1\n"
-            		+"T:Piece No.1\n"
+            		+"T:Pi124 -No.1\n"
             		+"M:4/4\n"
             		+"L:1/4\n"
             		+"Q:1/4=140\n"
@@ -11,7 +11,7 @@ public class Main{
             		+"C C C C C | E3/4 D/4 E3/4 F/4 G2 |]\n";
         String abc2 = "C C C3/4 D/4 E | E3/4 D/4 E3/4 F/4 G2 |]";
         
-        AbcMusic sample = AbcMusic.parse("C C C");
+        AbcMusic sample = AbcMusic.parse(abc);
 	}
 }
 
